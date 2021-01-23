1b7a4605e125526a5c345257d59b3fc4f939ea4f
Gragit <gme2468@gmail.com>
2021-01-23 12:57:06 +0800

    added basename to router in app.js

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <Router>
+    <Router basename="employee_directory">
       <div>
         <Header />
         <Wrapper>
